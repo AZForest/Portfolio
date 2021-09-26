534ce6745279e1ca6627f3b80e570400a778888e
Alex Forest <azforest44@gmail.com>
2021-09-26 01:22:36 -0700

    components

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import classes from './Footer.module.css';
+import resume from '../../assets/documents/AlexForestResume.pdf';
+
+const footer = () => {
+    return (
+        <div className={classes.Footer}>
+            <div className={classes.left}>
+                <a href="https://github.com/AZForest" target="_blank" rel="noopener noreferrer">Github</a>
+                <a href="https://www.linkedin.com/in/alex-forest" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+                <a href={resume} download>Resume</a>
+            </div>
+            <div className={classes.right}>
+                <p>Site By ALEX FOREST</p>
+            </div>
+        </div>
+    )
+}
+
+export default footer;
